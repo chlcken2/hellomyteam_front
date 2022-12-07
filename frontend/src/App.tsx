e437972642474f6ed69bf27a4590b0f4a3e6c870
sh981013s <sh981013s@gmail.com>
2022-12-08 01:11:56 +0900

    feat(*): prettier & eslint code formatter 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App: React.FC = () => {
-   const [hello, setHello] = useState('')
+  const [hello, setHello] = useState('')
+  const API_URL = process.env.REACT_APP_DEV_API_URL
 
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
+  useEffect(() => {
+    console.log(API_URL, '?????')
+    axios.get('/api/hello')
+      .then(response => setHello(response.data))
+      .catch(error => console.log(error))
+  }, [])
 
-    return (
+  return (
         <div>
             백엔드에서 가져온 데이터입니다 : {hello}
         </div>
-    );
+  )
 }
 
-export default App;
+export default App

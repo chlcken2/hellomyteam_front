77c6a87f1428d51745335116b75d08e8c67cb748
chlcken2 <ckdgus988@naver.com>
2022-12-07 18:09:34 +0900

    test : api서버 호출 테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
    const [hello, setHello] = useState('')
 
     useEffect(() => {
-        axios.get('/api/hello')
+        axios.get('https://hellomyteam-chlcken2.koyeb.app/api/hello')
         .then(response => setHello(response.data))
         .catch(error => console.log(error))
     }, []);

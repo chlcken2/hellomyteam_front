b41bce65df064a1fb6e048d15e374d6db60deff0
sh981013s <sh981013s@gmail.com>
2022-12-08 01:23:50 +0900

    feat(app): 기본 fetching test code 작성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { DEV_BASE_URL, PROD_BASE_URL } from './constants/urls';
 
 const App: React.FC = () => {
   const [hello, setHello] = useState('');
-  const API_URL = process.env.REACT_APP_DEV_API_URL;
 
-  useEffect(() => {
-    console.log(API_URL, '?????');
-    axios
-      .get('/api/hello')
-      .then((response) => setHello(response.data))
-      .catch((error) => console.log(error));
-  }, []);
+  const defaultFetchFunc = (): void => {
+    if (DEV_BASE_URL) {
+      axios
+        .get(`${DEV_BASE_URL}/api/hello`)
+        .then((response) => setHello(response.data))
+        .catch((error) => console.log(error));
+    }
+  };
 
-  return <div>백엔드에서 가져온 데이터입니다 : {hello}</div>;
+  return (
+    <>
+      <button onClick={defaultFetchFunc}>Fetch</button>
+      {hello && <div>백엔드에서 가져온 데이터입니다 : {hello}</div>}
+    </>
+  );
 };
 
 export default App;

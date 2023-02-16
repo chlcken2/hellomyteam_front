566fdc433c88499612154f2c441a580ede1386c3
chanyoung <kipa1003@naver.com>
2023-02-16 17:49:53 +0900

    feat: input component

---
+++ frontend/src/components/Input/Input.tsx
@@ -0,0 +1,55 @@
+import { memo, useEffect, useState } from "react";
+import "styles/components/input.scss";
+
+interface Props {
+  label: string;
+  isRequierd?: boolean;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  errorMessage?: string;
+  type?: React.HTMLInputTypeAttribute;
+  id?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  placeholder?: string;
+}
+
+const Input = ({
+  label,
+  isRequierd,
+  value,
+  setValue,
+  errorMessage,
+  type,
+  id,
+  onChange,
+  placeholder,
+}: Props) => {
+  const [isError, setIsError] = useState(false);
+  const inputContainerClassName = isError ? "input-container error" : "input-container";
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    setIsError(errorMessage?.length > 0);
+  }, [errorMessage]);
+
+  return (
+    <div className={inputContainerClassName}>
+      <div>
+        {label}
+        {isRequierd && "*"}
+      </div>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        onChange={onChange || handleChange}
+        placeholder={placeholder}
+      />
+      {isError && <p>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default memo(Input);

b34ff22cdbbdb9a6bba11e2af556826e4b6df2d4
chanyoung <kipa1003@naver.com>
2023-03-23 16:14:53 +0900

    fix: 변수명 네이밍 수정

--- frontend/src/layouts/Main.tsx
+++ frontend/src/layouts/Main.tsx
@@ -18,31 +18,39 @@ const MENU = [
 
 const Main = () => {
   const navi = useNavigate();
+  const { pathname } = useLocation();
+  const menuRef = useRef<HTMLDivElement>(null);
+
   const [useUser, setUseUser] = useRecoilState(UserState);
-  const [show, setShow] = useState(false);
-  // user의 id임. **team id가 XX!!!
+  const [showTeamsModal, setShowTeamsModal] = useState(false);
+
+  // 모바일 탭바의 menuItem 배경 인터렉션관련 스타일 state
+  const [menuItemBackgroundStyle, setMenuItemBackgroundStyle] = useState({
+    width: 0,
+    offsetLeft: 0,
+  });
   const [userId, setUserId] = useState(0);
-  // user team title
-  const [userTitle, setUserTitle] = useState("헬로마이팀");
-  // team id
-  const [teamId, setTeamId] = useState(0);
-  // 특정 user가 가입한 team
-  const { data: team, isLoading: load } = getTeamInfo(userId);
+  const [currentTeamTitle, setCurrentTeamTitle] = useState("헬로마이팀");
+  const [currentTeamId, setCurrentTeamId] = useState(0);
+
+  // User가 가입한 team list fetch
+  const { data: team, isLoading: isGetTeamInfoLoading } = getTeamInfo(userId);
 
   // 토글보이기
-  const handleTeams = () => {
-    setShow(!show);
+  const handleTeamsModal = () => {
+    setShowTeamsModal(!showTeamsModal);
   };
+
   // 팀글쓰기로 빠짐
   const handleTeamWrite = (teamId: number) => {
-    if (userTitle === "헬로마이팀") return alert("팀을 선택하세요");
+    if (currentTeamTitle === "헬로마이팀") return alert("팀을 선택하세요");
     navi(`/board/${teamId}/write`);
   };
   // 타이틀바꾸기
   const handleMember = (name: string, id: number) => {
-    setUserTitle(name);
-    setTeamId(id);
-    setShow(false);
+    setCurrentTeamTitle(name);
+    setCurrentTeamId(id);
+    setShowTeamsModal(false);
   };
 
   // recoil에 담긴 User의 정보가 있을시에, 사용자의 id값을 리액트 쿼리에 보냄
@@ -54,24 +62,14 @@ const Main = () => {
 
   // 리코일에 사용자 정보와 사용자가 가입한 팀을 모두 담는다
   useEffect(() => {
-    if (load) return;
-    if (team.data) {
+    if (team?.data) {
       setUseUser({ ...useUser, teamInfo: [...team.data] });
     }
   }, [team]);
 
-  const { pathname } = useLocation();
-  const menuRef = useRef<HTMLDivElement>(null);
-
-  // 모바일 탭바의 menuItem 배경 인터렉션관련 스타일 state
-  const [menuItemBackgroundStyle, setMenuItemBackgroundStyle] = useState({
-    width: 0,
-    offsetLeft: 0,
-  });
-
-  const menuClassName = (path: string) => {
-    if (path === "" && pathname === "/") return "active";
-    if (path !== "" && pathname.indexOf(path) !== -1) return "active";
+  const menuClassName = (menuPath: string) => {
+    if (menuPath === "" && pathname === "/") return "active";
+    if (menuPath !== "" && pathname.indexOf(menuPath) !== -1) return "active";
     return "";
   };
 
@@ -118,15 +116,19 @@ const Main = () => {
     <div className="main-wrap">
       <div className="main-buttons">
         <h1 className="main-title">
-          <button onClick={handleTeams}>{userTitle}</button>
+          <button onClick={handleTeamsModal}>{currentTeamTitle}</button>
         </h1>
         <ul>
           <li>
-            <Button text="글쓰기" handler={() => handleTeamWrite(teamId)} color="blue" />
+            <Button
+              text="글쓰기"
+              handler={() => handleTeamWrite(currentTeamId)}
+              color="blue"
+            />
           </li>
         </ul>
       </div>
-      {show && (
+      {showTeamsModal && (
         <div className="main-teams">
           <ul>
             {team?.data.map((el, idx) => {

0dc22143b4b2ec794c23021050c4d3a2ca428a70
chanyoung <kipa1003@naver.com>
2023-03-23 15:13:19 +0900

    fix: main layout 인터랙션

--- frontend/src/layouts/Main.tsx
+++ frontend/src/layouts/Main.tsx
@@ -1,12 +1,10 @@
-import React, { FC, useEffect, useState } from "react";
-import axios from "axios";
-import { Routes, Route, Link, NavLink, Outlet } from "react-router-dom";
-import { useRecoilState, useRecoilValue } from "recoil";
-import LoginState from "recoil/atom";
-import UserState from "recoil/userAtom";
+import { useEffect, useRef, useState } from "react";
+import { Link, Outlet, useLocation } from "react-router-dom";
+
 import "styles/pages/home.scss";
 import "styles/layouts/main.scss";
 
+const BREAK_POINT = 1000;
 const MENU = [
   { path: "", name: "둘러보기" },
   { path: "notice", name: "공지게시판" },
@@ -16,26 +14,73 @@ const MENU = [
 ];
 
 const Main = () => {
-  const [on, setOn] = useState<number>(0);
-  const [confirmLogin, setConfirmLogin] = useRecoilState(LoginState);
-  const useUser = useRecoilValue(UserState);
+  const { pathname } = useLocation();
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  // 모바일 탭바의 menuItem 배경 인터렉션관련 스타일 state
+  const [menuItemBackgroundStyle, setMenuItemBackgroundStyle] = useState({
+    width: 0,
+    offsetLeft: 0,
+  });
+
+  const menuClassName = (path: string) => {
+    if (path === "" && pathname === "/") return "active";
+    if (path !== "" && pathname.indexOf(path) !== -1) return "active";
+    return "";
+  };
+
+  const handleMenuItemInteraction = () => {
+    const screenWidth = window.innerWidth;
+    const activeMenu = menuRef.current?.querySelector(".active");
+
+    if (activeMenu && screenWidth < BREAK_POINT) {
+      const activeIndex = MENU.findIndex((item) => {
+        return (
+          (item.path === "" && pathname === "/") ||
+          (item.path !== "" && pathname.indexOf(item.path) !== -1)
+        );
+      });
+
+      if (activeMenu) {
+        menuRef.current?.scrollTo({
+          left: 100 * activeIndex - 24,
+          behavior: "smooth",
+        });
+
+        const { offsetLeft } = activeMenu as HTMLLIElement;
+        const { width } = activeMenu.getBoundingClientRect();
+        setMenuItemBackgroundStyle({ width, offsetLeft });
+      } else setMenuItemBackgroundStyle({ width: 0, offsetLeft: 0 });
+    }
+  };
 
   useEffect(() => {
-    console.log(useUser);
-  });
+    if (pathname) {
+      handleMenuItemInteraction();
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleMenuItemInteraction);
+    return () => {
+      window.removeEventListener("resize", handleMenuItemInteraction);
+    };
+  }, []);
 
   return (
     <div className="main-wrap">
       <h1 className="main-title">우리동네 축구팀</h1>
-      <div className="main-menu-wrapper">
+      <div ref={menuRef} className="main-menu-wrapper">
         <ul className="main-menu">
+          <div
+            className="active-item-background"
+            style={{
+              transform: `translateX(${menuItemBackgroundStyle.offsetLeft}px)`,
+              width: `${menuItemBackgroundStyle.width}px`,
+            }}
+          />
           {MENU.map((menuItem, idx) => (
-            <li
-              key={idx}
-              onClick={() => setOn(idx)}
-              onKeyDown={() => setOn(idx)}
-              className={on === idx ? "active" : ""}
-            >
+            <li key={idx} className={menuClassName(menuItem.path)}>
               <Link to={`/${menuItem.path}`}>{menuItem.name}</Link>
             </li>
           ))}

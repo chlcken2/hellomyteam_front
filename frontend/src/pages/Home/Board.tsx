01375484d74a088623c95f828252923fff8489e5
hayounglee <home124@naver.com>
2023-05-10 14:26:19 +0900

    feat: 검색 ui

--- frontend/src/pages/Home/Board.tsx
+++ frontend/src/pages/Home/Board.tsx
@@ -5,6 +5,8 @@ import getBoardList from "quires/board/getBoardList";
 import Pagination from "components/common/Pagination";
 import UserState from "recoil/userAtom";
 import { useRecoilValue } from "recoil";
+import Select from "components/common/Select";
+import Input from "components/common/Input";
 
 const Board: FC = () => {
   const path = process.env.PUBLIC_URL;
@@ -17,7 +19,14 @@ const Board: FC = () => {
   const [idx, setIdx] = useState(0);
 
   const [sortType, setSortType] = useState("created_date");
+  const [flag, setFlag] = useState(false);
 
+  const [boardName, setBoardName] = useState({
+    label: "자유게시판",
+    value: "FREE_BOARD",
+  });
+
+  const [inputValue, setInputValue] = useState("");
   // (4/27) selectedTeamId가 없을 경우 localStorage에서 가져오게
   const {
     data: list,
@@ -41,54 +50,78 @@ const Board: FC = () => {
   }, [item, sortType]);
 
   console.log(user);
+  const option = [
+    { label: "자유게시판", value: "FREE_BOARD" },
+    { label: "공지게시판", value: "NOTICE_BOARD" },
+  ];
 
   return (
     <div>
       <section className="section-container">
         <div className="section-top">
           <h2>자유게시판</h2>
-          <div className="option-box">
-            <div className="sort-box">
-              <button className="sort-type">
-                <img src={`${path}/common/board-list.png`} alt="arrow-up" />
-              </button>
-              <div className="main-teams__wrap board-list">
-                <div className="main-teams">
-                  <ul>
-                    <li className={idx === 0 ? "on" : ""}>
-                      <button
-                        onClick={() => {
-                          setIdx(0);
-                          setSortType("created_date");
-                        }}
-                      >
-                        최신순
-                      </button>
-                    </li>
-                    <li className={idx === 1 ? "on" : ""}>
-                      <button
-                        onClick={() => {
-                          setIdx(1);
-                          setSortType("like_count");
-                        }}
-                      >
-                        좋아요순
-                      </button>
-                    </li>
-                    <li className={idx === 2 ? "on" : ""}>
-                      <button
-                        onClick={() => {
-                          setIdx(2);
-                          setSortType("like_count");
-                        }}
-                      >
-                        오래된순
-                      </button>
-                    </li>
-                  </ul>
+          <div className="option-box board-input">
+            <ul>
+              <li>
+                <img src={`${path}/icons/board-search.png`} alt="" />
+              </li>
+              <li>
+                <Select
+                  placeholder="자유게시판"
+                  options={option}
+                  defaultValue="FREE_BOARD"
+                  onChange={(e) => setBoardName(e)}
+                />
+              </li>
+              <li>
+                <Input value={inputValue} />
+              </li>
+              <li>
+                <div className="sort-box">
+                  <button className="sort-type" onClick={() => setFlag(!flag)}>
+                    <img src={`${path}/common/board-list.png`} alt="arrow-up" />
+                  </button>
+                  {flag && (
+                    <div className="main-teams__wrap board-list">
+                      <div className="main-teams">
+                        <ul>
+                          <li className={idx === 0 ? "on" : ""}>
+                            <button
+                              onClick={() => {
+                                setIdx(0);
+                                setSortType("created_date");
+                              }}
+                            >
+                              최신순
+                            </button>
+                          </li>
+                          <li className={idx === 1 ? "on" : ""}>
+                            <button
+                              onClick={() => {
+                                setIdx(1);
+                                setSortType("like_count");
+                              }}
+                            >
+                              좋아요순
+                            </button>
+                          </li>
+                          <li className={idx === 2 ? "on" : ""}>
+                            <button
+                              onClick={() => {
+                                setIdx(2);
+                                setSortType("like_count");
+                              }}
+                            >
+                              오래된순
+                            </button>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  )}
                 </div>
-              </div>
-            </div>
+              </li>
+            </ul>
           </div>
         </div>
         <ul className="post-list">

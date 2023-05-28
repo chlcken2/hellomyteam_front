839996ccb9103988350906c85d9a96900b3424a3
chanyoung <kipa1003@naver.com>
2023-05-28 18:48:49 +0900

    test: api connection

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -45,7 +45,7 @@ const getRefreshToken = async (): Promise<string | void> => {
 
 const createInstance = () => {
   return axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://localhost:3000",
     timeout: 9000,
     withCredentials: true,
     headers: { "Content-Type": "application/json" },

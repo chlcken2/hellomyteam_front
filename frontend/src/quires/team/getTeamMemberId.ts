cc06f04fa85369ad103906c5f86f11ef3cc4a734
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2023-06-03 00:17:18 +0900

    myTeams

--- frontend/src/quires/team/getTeamMemberId.ts
+++ frontend/src/quires/team/getTeamMemberId.ts
@@ -19,8 +19,10 @@ export const useTeamInfoFetcher = (teamId: number, memberId: number) =>
  * 로그인 후 가입한 팀 id와 팀 이름 가져오기
  * @returns
  */
-const teamMemberId = (teamId: number, memberId: number) => {
-  return useQuery([QUERY_KEY], () => useTeamInfoFetcher(teamId, memberId));
+const teamMemberId = (teamId: number, memberId: number, enabled = true) => {
+  return useQuery([QUERY_KEY], () => useTeamInfoFetcher(teamId, memberId), {
+    enabled,
+  });
 };
 
 export default teamMemberId;

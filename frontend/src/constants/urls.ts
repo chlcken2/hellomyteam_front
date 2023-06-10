bb1488279430031f40c6b1819f329b28fd67111c
chanyoung <kipa1003@naver.com>
2023-06-10 20:36:59 +0900

    fix: API URL

--- frontend/src/constants/urls.ts
+++ frontend/src/constants/urls.ts
@@ -1,4 +1,3 @@
 export const DEV_BASE_URL = process.env.REACT_APP_DEV_API_URL;
 export const PROD_BASE_URL = process.env.REACT_APP_PROD_API_URL;
-export const API_URL =
-  "http://ec2-3-38-108-158.ap-northeast-2.compute.amazonaws.com:8080";
+export const API_URL = "https:hellomyteam.store";
